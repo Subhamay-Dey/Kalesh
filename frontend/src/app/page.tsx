6048325773722c60398d07e59afbbebd013abe5b
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-07 00:40:19 +0530

    Update - Next js full Setup done in frontend folder and also intrigated shadcn

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">I am Home Page</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
