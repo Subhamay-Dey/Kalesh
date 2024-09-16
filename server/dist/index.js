cc435a0f88bccc44e7e61188ddacaa1360561f97
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-16 19:31:03 +0530

    Major update - View Engine setup done ✅ and the ejs code is running properly in server, used copyfiles package to copy the ejs code in src to the dist folder as I am using typescript.

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,7 +2,14 @@ import express from "express";
 import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 8000;
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get("/", (req, res) => {
-    return res.send("The server is working... 🤗");
+    return res.render("welcome");
 });
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,18 @@ const app:Application = express();
 
 const PORT = process.env.PORT || 8000;
 
+import path from "path"
+import {fileURLToPath} from "url"
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve(__dirname, "./views"))
+
 app.get("/", (req:Request, res:Response) => {
-    return res.send("The server is working... 🤗")
+    return res.render("welcome")
 })
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
