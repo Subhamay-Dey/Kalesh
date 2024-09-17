0ae50ad572416ef93f875e663a14926f779ecb19
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-17 23:59:18 +0530

    using axios .

--- frontend/src/actions/authActions.ts
+++ frontend/src/actions/authActions.ts
@@ -1,5 +1,16 @@
 "use server"
+import { REGISTER_URL } from "@/lib/apiEndPoints"
+import axios from "axios"
 
 export async function registerAction(prevState:any, formdata: FormData) {
-    
+    try {
+        await axios.post(REGISTER_URL, {
+            name: formdata.get("name"),
+            email: formdata.get("email"),
+            password: formdata.get("password"),
+            confirm_password: formdata.get("confirm_password"),
+        })
+    } catch (error) {
+        
+    }
 }
