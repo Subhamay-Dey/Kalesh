e3c4919e7c59adc0a7d05c4b1a5716d22e733ea2
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-20 23:59:49 +0530

    forget-password completed.

--- server/dist/routes/passwordRoutes.js
+++ server/dist/routes/passwordRoutes.js
@@ -4,6 +4,8 @@ import { authLimitter } from "../config/rateLimit.js";
 import { ZodError } from "zod";
 import { formatError } from "../helper.js";
 import { forgetPasswordSchema } from "../validation/passwordValidation.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid4 } from "uuid";
 const router = Router();
 router.post("/forget-password", authLimitter, async (req, res) => {
     try {
@@ -16,6 +18,18 @@ router.post("/forget-password", authLimitter, async (req, res) => {
                     email: "No user found with this email."
                 } });
         }
+        const salt = await bcrypt.genSalt(10);
+        const token = await bcrypt.hash(uuid4(), salt);
+        await prisma.user.update({
+            data: {
+                password_reset_token: token,
+                token_send_at: new Date().toISOString()
+            },
+            where: {
+                email: payload.email
+            },
+        });
+        const url = `${process.env.CLIENT_APP_URL}/reset-password?email=${payload.email}&token=${token}`;
     }
     catch (error) {
         if (error instanceof ZodError) {
--- server/src/routes/passwordRoutes.ts
+++ server/src/routes/passwordRoutes.ts
@@ -2,8 +2,11 @@ import { Router, Request, Response } from "express";
 import prisma from "../config/databse.js";
 import { authLimitter } from "../config/rateLimit.js";
 import { ZodError } from "zod";
-import { formatError } from "../helper.js";
+import { formatError, renderEmailEjs } from "../helper.js";
 import { forgetPasswordSchema } from "../validation/passwordValidation.js";
+import bcrypt from "bcrypt";
+import {v4 as uuid4} from "uuid";
+import { emailQueue, emailQueueName } from "../jobs/EmailJob.js";
 
 const router = Router()
 
@@ -22,6 +25,30 @@ router.post("/forget-password", authLimitter, async (req:Request, res: Response)
                 email: "No user found with this email."
             }});
         }
+
+        const salt = await bcrypt.genSalt(10)
+        const token = await bcrypt.hash(uuid4(), salt)
+
+        await prisma.user.update({
+            data:{
+                password_reset_token: token,
+                token_send_at: new Date().toISOString()
+            },
+            where:{
+                email:payload.email
+            },
+        })
+
+        const url = `${process.env.CLIENT_APP_URL}/reset-password?email=${payload.email}&token=${token}`
+
+        const html = await renderEmailEjs("forget-password", {url: url})
+        await emailQueue.add(emailQueueName, {
+            to: payload.email,
+            subject: "Reset your password",
+            body: html
+        })
+
+        return res.json({message: "Email sent successfully! please checkyour email."})
 
     } catch (error) {
         if(error instanceof ZodError) {
