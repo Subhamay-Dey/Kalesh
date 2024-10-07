c1da809d77a0e2bf59c69b9792e06999edfad4e3
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-08 03:28:39 +0530

    Add kalesh full form ui done - added the image file input and also the expire date of the kalesh and the form submit button ✅.

--- frontend/src/components/kalesh/AddKalesh.tsx
+++ frontend/src/components/kalesh/AddKalesh.tsx
@@ -7,14 +7,26 @@ import {
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-import { Button } from '../ui/button';
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import { Textarea } from "@/components/ui/textarea"
 
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+ 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
 function AddKalesh() {
     const [open, setOpen] = useState(false)
     const [kaleshdata, setKaleshData] = useState<KaleshFormType>({})
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <Dialog open={open} onOpenChange={setOpen} >
     <DialogTrigger asChild>
@@ -53,8 +65,32 @@ function AddKalesh() {
             </div>
 
             <div className='mt-4'>
-                <Label htmlFor='expireAt'>Expire At</Label>
-                <Input id='title' placeholder='Enter your title here...' value={kaleshdata?.title ?? ""} onChange={(e) => setKaleshData({...kaleshdata, title:e.target.value})}/>
+                <Label htmlFor='expireAt' className='block'>Expires At</Label>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full mt-2 justify-start text-left font-normal",
+                        !date && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0">
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={setDate}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+            </div>
+            <div className='mt-6'>
+              <Button className='w-full'>Submit</Button>
             </div>
         </form>
     </DialogContent>
